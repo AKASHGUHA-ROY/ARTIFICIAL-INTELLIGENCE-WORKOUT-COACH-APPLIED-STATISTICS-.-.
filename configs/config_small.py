2b44804f93f421a4582d1f2dc5d1daf3172dac8b
AKASHGUHA-ROY <akashguharoy651@gmail.com>
2026-03-25 16:57:08 -0700

    Update config_small.py

--- configs/config_small.py
+++ configs/config_small.py
@@ -1,1 +1,12 @@
-# Small config placeholder
+config = {
+    "batch_size": 32,
+    "block_size": 64,
+    "n_embd": 64,
+    "n_head": 2,
+    "n_layer": 2,
+    "dropout": 0.1,
+    "learning_rate": 3e-4,
+    "max_iters": 2000,
+    "eval_interval": 200,
+    "eval_iters": 50,
+}
